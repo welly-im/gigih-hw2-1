75d4d4a64fbc85c129085b944a68d2ca75da7e9e
welly-im <wellyimanuel86@gmail.com>
2022-03-22 15:45:43 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Data from './data/data';
 
 function App() {
+
+  let images = Data.album.images[0].url;
+  let titleSong = Data.album.name;
+  let artist = Data.album.artists[0].name;
+  let url = Data.album.external_urls.spotify;
+  console.log(Data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Playlist">
+      <div className='Image'>
+      <img src={images}/>
+      </div>
+      <div className='Playlist-info'>
+        <h4>Album</h4>
+        <p className='title-song'>{titleSong}</p>
+        <p className='artist'>{artist}</p>
+        <a href={url} target='_blank'><button className='btn-select'>Select</button></a> 
+      </div>
     </div>
   );
 }
